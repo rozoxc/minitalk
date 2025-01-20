7b63d47aa334c17b1ca5b35d8afe7bab2e8118c8
Abderrahmane Abdoullah <ababdoul@e2r7p8.1337.ma>
2025-01-21 00:38:31 +0100

    sl

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: ababdoul <ababdoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 21:35:01 by ababdoul          #+#    #+#             */
-/*   Updated: 2025/01/18 00:46:16 by ababdoul         ###   ########.fr       */
+/*   Updated: 2025/01/21 00:22:30 by ababdoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	send_char(pid_t pid, char c)
 	int	bit;
 
 	bit = 0;
-	usleep(25);
 	while (bit < 8)
 	{
 		if (c & (1 << bit))
@@ -28,7 +27,31 @@ void	send_char(pid_t pid, char c)
 		bit++;
 	}
 }
+int check_av(char *str)
+{
+	int	i;
+	int	found_digit;
 
+	i = 0;
+	found_digit = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] == ' ')
+			i++;
+		if (!str[i])
+			break ;
+		if (str[i] == '-' || str[i] == '+')
+			i++;
+		if (!str[i] || str[i] < '0' || str[i] > '9')
+			return (0);
+		found_digit = 1;
+		while (str[i] && str[i] >= '0' && str[i] <= '9')
+			i++;
+		if (str[i] == '-' || str[i] == '+')
+			return (0);
+	}
+	return (found_digit);
+}
 int	main(int argc, char **argv)
 {
 	pid_t	server_pid;
@@ -38,9 +61,15 @@ int	main(int argc, char **argv)
 	if (argc != 3)
 	{
 		ft_printf("Usage: %s <server_pid> <message>\n", argv[0]);
-		ft_printf("Example : ./client 2222 \"your _message ;)\" ");
+		ft_printf("Example : ./client 2222 \"your_message ;)\" ");
 		return (1);
 	}
+	if (check_av(argv[1]) == 0)
+	{
+		ft_printf("invalid pid :( \n");
+		return (1);
+	}
+	
 	server_pid = ft_atoi(argv[1]);
 	message = argv[2];
 	i = 0;
--- server.c
+++ server.c
@@ -6,40 +6,58 @@
 /*   By: ababdoul <ababdoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 21:35:07 by ababdoul          #+#    #+#             */
-/*   Updated: 2025/01/18 00:49:17 by ababdoul         ###   ########.fr       */
+/*   Updated: 2025/01/21 00:34:50 by ababdoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	handle_signal(int signal)
+void handle_signal(int signal, siginfo_t *info , void *content)
 {
-	static int	bit = 0;
-	static char	c = 0;
+    static int bit = 0;
+    static char c = 0;
+    static pid_t pid;
 
-	if (signal == SIGUSR1)
-		c |= (1 << bit);
-	bit++;
-	if (bit == 8)
-	{
-		write(1, &c, 1);
-		bit = 0;
-		c = 0;
-	}
+	(void)content;
+    if (pid == 0)
+        pid = info->si_pid;
+    if (info->si_pid != pid)
+    {
+        pid = 0;
+        bit = 0;
+        c = 0;
+    }
+    if (signal == SIGUSR1)
+    {
+        c |= (1 << bit);
+    }
+    bit++;
+    if (bit == 8)
+    {
+        write(1, &c, 1);
+        bit = 0;
+        c = 0;
+    }
 }
 
-int	main(void)
+int main(int ac, char **av)
 {
-	struct sigaction	sa;
+    struct sigaction sa;
+    (void)av;
 
-	write(1, "Server PID: ", 11);
-	ft_printf("%d\n", getpid());
-	sa.sa_handler = handle_signal;
-	sa.sa_flags = 0;
-	sigemptyset(&sa.sa_mask);
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-	while (1)
-		pause();
-	return (0);
+    if (ac != 1)
+    {
+        ft_printf("error !!");
+        return (1);
+    }
+    ft_printf("PID : ");
+    ft_printf("%d\n", getpid());
+
+    sa.sa_sigaction = handle_signal;
+    sa.sa_flags = SA_SIGINFO;
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+    while (1)
+        pause();
+    return (0);
 }
